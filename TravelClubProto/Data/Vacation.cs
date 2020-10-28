0dd6f091e324495cb5071a3852205293a66d845c
DESKTOP-M74EIJ9\johan <jthoms19@student.aau.dk>
2020-10-28 10:49:42 +0100

    Init COmmit

---
+++ TravelClubProto/Data/Vacation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelClubProto.Data
+{
+    public class Vacation
+    {
+        public string Type { get; set; }
+        public string Destination { get; set; }
+        public int Price { get; set; }
+
+        public Vacation(string type, string destination, int price)
+        {
+            Type = type;
+            Destination = destination;
+            Price = price;
+        }
+    }
+}
---
+++ TravelClubProto/Data/VacationList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelClubProto.Data
+{
+    public class VacationAdmin
+    {
+        public int Age = 10;
+
+        public List<Vacation> PublishedVacations { get; } = new List<Vacation>();
+
+        public void PublishVacation(Vacation newVac)
+        {
+            PublishedVacations.Add(newVac);
+        }
+    }
+}
