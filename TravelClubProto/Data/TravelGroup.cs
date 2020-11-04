ea24720a1833ece0c8d970899ad1ab43ae02c1b2
Zzeeker <Rasmus.rbs@gmail.com>
2020-11-04 09:30:01 +0100

    Mostly Idle with a non-functional autoclicker, Counter will be moved to different branch from now

---
+++ TravelClubProto/Data/TravelGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelClubProto.Data
+{
+    public class TravelGroup
+    {
+        List<Customer> JoinedUsers;
+        List<Customer> FavouritedUsers;
+
+        private void SendNotification()
+        {
+            //:q-ping:
+        }
+    }
+}
