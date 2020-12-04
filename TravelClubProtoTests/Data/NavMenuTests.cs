f2c99839d90a57c86e8c7b12816d2e5286d4b760
Zzeeker <Rasmus.rbs@gmail.com>
2020-12-04 09:49:29 +0100

    Basic intergration testing

---
+++ TravelClubProtoTests/Data/NavMenuTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelClubProto.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
+using Microsoft.Extensions.Configuration;
+using BlazorStrap;
+
+namespace TravelClubProto.Data.Tests
+{
+    [TestClass()]
+    public class NavMenuTests
+    {
+        [TestMethod()]
+        public void HamburgerMenuOpenTest()
+        {
+            using var ctx = new Bunit.TestContext();
+            InitialiseTestContext(ctx);
+            var cut = ctx.RenderComponent<NavMenu>();
+
+
+            cut.Find(".btn.dropdown-toggle.oi.oi-menu").Click();
+            cut.Find(".dropdown-menu.dropdown-menu-right.show");
+        }
+
+        [TestMethod()]
+        public void HamburgerMenuCloseTest()
+        {
+            using var ctx = new Bunit.TestContext();
+            InitialiseTestContext(ctx);
+            var cut = ctx.RenderComponent<NavMenu>();
+
+
+            cut.Find(".btn.dropdown-toggle.oi.oi-menu").Click();
+            cut.Find(".btn.dropdown-toggle.oi.oi-menu").Click();
+
+            ElementNotFoundException ex = Assert.ThrowsException<ElementNotFoundException>(() => cut.Find(".dropdown-menu.dropdown-menu-right.show"));
+            Assert.IsNotNull(ex);
+        }
+
+        [TestMethod()]
+        public void LoginModalOpenTest()
+        {
+            using var ctx = new Bunit.TestContext();
+            InitialiseTestContext(ctx);
+            var cut = ctx.RenderComponent<NavMenu>();
+
+            cut.Find(".btn.dropdown-toggle.oi.oi-menu").Click();
+            cut.Find("a.dropdown-item").Click();
+            cut.Find(".modal-dialog.modal-dialog-centered");
+        }
+
+        [TestMethod()]
+        public void LoginModalCloseTest()
+        {
+            using var ctx = new Bunit.TestContext();
+            InitialiseTestContext(ctx);
+            var cut = ctx.RenderComponent<NavMenu>();
+
+            cut.Find(".btn.dropdown-toggle.oi.oi-menu").Click();
+            cut.Find("a.dropdown-item").Click();
+            cut.Find("button.close").Click();
+
+            ElementNotFoundException ex = Assert.ThrowsException<ElementNotFoundException>(() => cut.Find("div.modal.fade.show"));
+            Assert.IsNotNull(ex);
+        }
+
+
+
+
+
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+            return config;
+        }
+
+        public IConfiguration config = InitConfiguration();
+
+        public void InitialiseTestContext(Bunit.TestContext ctx)
+        {
+
+            ctx.Services.AddBlazoredLocalStorage();
+            ctx.Services.AddSingleton<DataAccessService>();
+            ctx.Services.AddSingleton<IConfiguration>(config);
+            ctx.Services.AddBootstrapCss();
+
+        }
+
+    }
+
+
+
+
+}
--- TravelClubProtoTests/Data/VacationTests.cs
+++ TravelClubProtoTests/Data/VacationTests.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace TravelClubProto.Data.Tests
+namespace TravelClubProtoTests.Data.Tests
 {
     [TestClass()]
     public class VacationTests
